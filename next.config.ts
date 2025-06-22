d7191c91a21e6f1deb4a814b752f50c8d8c8d251
Sunidhi23garg <sgarg8_be21@thapar.edu>
2025-06-23 01:12:31 +0530

    Fix: allow external image domains

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['utfs.io', 'api.dicebear.com'],
+  },
 };
 
 module.exports = nextConfig;
 
-// import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
 // };
 
-// export default nextConfig;
+// module.exports = nextConfig;
+
